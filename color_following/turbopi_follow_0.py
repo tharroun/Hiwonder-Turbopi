77c6f01f41082386b8ab62bfdc19fb07a17e4e99
tharroun <tharroun@gmail.com>
2025-06-08 17:04:35 -0400

    Changed to three-threads; vision, servo control, and mecanum.

--- color_following/turbopi_follow_0.py
+++ color_following/turbopi_follow_0.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/hiwonder/Github/Hiwonder-Turbopi')
+sys.path.append('/home/hiwonder/Github/Hiwonder-Turbopi/sdk')
 import time
 import threading
 from queue import Queue
@@ -5,9 +10,12 @@ import yaml
 import numpy
 import mypid
 import hw_sdk_robot
+import hw_sdk_sonar
+import tah_sdk_mecanum
 import cv2
 
 KILL_THREAD = object() 
+LOST_OBJECT = object()
 
 # -------------
 # LOAD THE COLOR DEFINITIONS
@@ -23,7 +31,7 @@ large_kernel   = numpy.ones((9, 9), numpy.uint8)
 
 servo2_center = 1500 # x (horizontal) direction
 servo1_center = 1500 # y (vertical) direction
-set_radius    = 40
+set_radius    = 40.0
 
 # ==============================================================
 def robot_servo(qs,robot) -> int:
@@ -36,7 +44,7 @@ def robot_servo(qs,robot) -> int:
 
     (width,height) = qs.get()
     qs.task_done()
-    assert (width,height)==(640,420) , print("First data in servo queue is not the width/height.")
+    assert (width,height)==(640,480) , print("First data in servo queue is not the width/height.")
     cX = width/2.0
     cY = height/2.0
 
@@ -49,6 +57,9 @@ def robot_servo(qs,robot) -> int:
             robot.pwm_servo_set_position(0.3, [[2, servo2_center]])
             qs.task_done()
             break
+        elif data is LOST_OBJECT:
+            robot.pwm_servo_set_position(0.3, [[1, servo1_center]])
+            robot.pwm_servo_set_position(0.3, [[2, servo2_center]])
         else :
             move_x = sx.pid(cX, data[0], data[3])
             new_x_position = int(servo_x_position + move_x)
@@ -60,7 +71,7 @@ def robot_servo(qs,robot) -> int:
             new_y_position = int(servo_y_position - move_y)
             if new_y_position < 1100 or new_y_position > 1900 : new_y_position = servo_y_position
             # ----------------------------------------------------------
-            robot.pwm_servo_set_position(0.05, [[1, new_y_position],[2, new_y_position]]) 
+            robot.pwm_servo_set_position(0.3, [[1, new_y_position],[2, new_x_position]]) 
             servo_x_position = new_x_position
             servo_y_position = new_y_position
         # ----------------------------------------------------------
@@ -71,47 +82,56 @@ def robot_servo(qs,robot) -> int:
 
 # ==============================================================
 def robot_mecanum(qm) -> int:
-    mr = mypid.mypid(0.005,0.0001,0.00005)
-    mz = mypid.mypid(0.005,0.0001,0.00005)
+    mr = mypid.mypid(0.0015,0.0,0.00005)
+    mz = mypid.mypid(0.004,0.0,0.0001)
 
     wheels = tah_sdk_mecanum.Mecanum(robot)
 
-    (width,height) = qs.get()
-    qs.task_done()
-    assert (width,height)==(640,420) , print("First data in servo queue is not the width/height.")
+    (width,height) = qm.get()
+    qm.task_done()
+    assert (width,height)==(640,480) , print("First data in servo queue is not the width/height.")
     cX = width/2.0
     cY = height/2.0
 
     while True:
-        data = qs.get()
+        data = qm.get()
         # ----------------------------------------------------------
         # THE TASK
         if data is KILL_THREAD:
             wheels.reset_motors()
-            qs.task_done()
+            qm.task_done()
             break
+        elif data is LOST_OBJECT:
+            wheels.reset_motors()
         else :
             # ----------------------------------------------------------
-            if data[0] < 1000 :
-                control = mr.pid(1000,data[0],data[3])
-                wheels.set_velocity(0,90,0.2)
-            elif data[0] > 2000:
-                control = mr.pid(2000,data[0],data[3])
+            if data[0] < 200 :
+                control = numpy.fabs(mr.pid(200,data[0],data[3]))
+                control = 0.12 if control > 0.12 else control
                 wheels.set_velocity(0,90,-0.2)
-            else : control = 0.0
-            time.sleep(numpy.fabs(control))
-            wheels.reset_motors()
+                time.sleep(control)
+                wheels.reset_motors()
+            if data[0] > 440:
+                control = numpy.fabs(mr.pid(440,data[0],data[3]))
+                control = 0.12 if control > 0.12 else control
+                wheels.set_velocity(0,90,0.2)
+                time.sleep(control)
+                wheels.reset_motors()
             # ----------------------------------------------------------
-            if numpy.fabs(data[2] - set_radius) > 15:
+            if numpy.fabs(data[2] - set_radius) > 10:
                 control = mz.pid(set_radius,data[2],data[3])
                 if control < 0 :
+                    control = 0.12 if numpy.fabs(control) > 0.12 else numpy.fabs(control)
                     wheels.set_velocity(20,180,0.0)
+                    time.sleep(control)
+                    wheels.reset_motors()
                 else:
+                    control = 0.12 if control > 0.12 else control
                     wheels.set_velocity(20,0,0.0)
-                time.sleep(numpy.fabs(control))
-            wheels.reset_motors()
+                    time.sleep(control)
+                    wheels.reset_motors()
         # ----------------------------------------------------------
-        qs.task_done()
+        qm.task_done()
 
     wheels.reset_motors()
     return 1 
@@ -151,26 +171,36 @@ def robot_see(qs,qm) -> int:
             t2 = time.perf_counter()
             if (len(contours)>1) : 
                 contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+            # --
+            area = cv2.contourArea(contours[0])
+            arclength = cv2.arcLength(contours[0],True)
+            circularity = 4*numpy.pi*area/(arclength*arclength)
+            # NEXT TIME:https://docs.opencv.org/4.11.0/d4/d70/tutorial_hough_circle.html
+            # --
             M = cv2.moments(contours[0])
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             radius = int(numpy.sqrt(M['m00']/numpy.pi))
-            qs.put((cx,cy,radius,t2-t1))
-            qm.put((cx,cy,radius,t2-t1))
-            cv2.circle(frame,(cx,cy),radius,(0,255,0),5)
+            if circularity < 0.7 : 
+                qs.put(LOST_OBJECT)
+                qm.put(LOST_OBJECT)
+            else:
+                qs.put((cx,cy,radius,t2-t1))
+                qm.put((cx,cy,radius,t2-t1))
+            #cv2.circle(frame,(cx,cy),radius,(0,255,0),5)
+            cv2.drawContours(frame,contours,0,(0,0,255),5)
             t1 = t2
+            # --
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(10) == ord('q'):
+            qs.put(KILL_THREAD)
+            qm.put(KILL_THREAD)
             break
 
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
-    print(f"Queue Servo   : {qs.size()}")
-    print(f"Queue Mecanum : {qm.size()}")
-    qs.put(KILL_THREAD)
-    qm.put(KILL_THREAD)
     return 1
 # ==============================================================
 
@@ -238,8 +268,9 @@ if __name__ == '__main__':
     thread_servo.start()
     thread_mecanum.start()
 
-    #thread_vision.join()
+    thread_vision.join()
 
     qs.join()
     qm.join()
-    print(qs.qsize(),qm.qsize())
+    print(f"Queue Servo   : {qs.qsize()}")
+    print(f"Queue Mecanum : {qm.qsize()}")
